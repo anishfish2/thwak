e3c754e48f854738105f18ac0e68100952ead8a8
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2025-04-01 19:27:12 -0500

    restarting from scratch

--- setup_keyboard_sound_task.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Setting up Keyboard Sound Task...
-schtasks /Create /SC ONLOGON /TN "KeyboardSound" /TR "'C:\Python310\python.exe' 'C:\Users\Owner\Desktop\Projects\thwak\giga.py'" /RL HIGHEST /F
-echo Task created successfully. The script will run when you log in.
-pause
